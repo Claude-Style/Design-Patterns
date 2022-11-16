662bef4105ba7fc94887f2fabcc738de3c698794
Claude Shao <cld.shao@gmail.com>
2022-11-16 18:06:01 -0500

    COMMIT 01 Proxy Simulate AOP

---
+++ 01 SingletonExamples/src/main/java/shao/hashnode/dev/Singleton1.java
@@ -0,0 +1,40 @@
+package shao.hashnode.dev;
+
+
+/**
+ * This is an example of Lazy Initialization Singleton
+ * @author Claude Shao
+ * http://shao.hashnode.dev
+ */
+public final class Singleton1 implements SingletonInterface{       //'final' prevent inner class created
+	
+	private static Singleton1 singletonObj;      //'static' prevent more than one object created
+
+	private Singleton1() {          //'private of constructor prevent subclasses inherit the constructor and prevent new the object
+	}
+	
+	public synchronized static Singleton1 getInstance()
+	{
+		if(singletonObj==null)
+		{
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			singletonObj = new Singleton1();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return singletonObj;
+		}
+		else
+		{
+			return singletonObj;
+		}		
+	}
+}
---
+++ 01 SingletonExamples/src/main/java/shao/hashnode/dev/TestNGMain.java
@@ -0,0 +1,68 @@
+package shao.hashnode.dev;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import shao.hashnode.dev.threads.TestThread;
+
+public class TestNGMain {
+
+  private SingletonInterface singletonObject1;
+  private SingletonInterface singletonObject2;
+  @Test
+  public void f() {
+	  if(singletonObject1==singletonObject2)
+	  {
+		  System.out.println("same object");
+		  System.out.println("singletonObj1="+singletonObject1);
+		  System.out.println("singletonObj2="+singletonObject2);
+	  }
+	  else
+	  {
+		  System.out.println("not same object");
+		  System.out.println("singletonObj1="+singletonObject1);
+		  System.out.println("singletonObj2="+singletonObject2);
+	  }
+	  
+	  /*
+	   * multi-thread testing case
+	   */
+	  TestThread tt = new TestThread();
+	  Thread thread1 = new Thread( tt, "thread1");
+	  Thread thread2 = new Thread( tt, "thread2");
+	  thread1.start();
+	  thread2.start();
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {	  
+//	  singletonObject = new Singleton1(); this doesn't work because the constructor is private	 
+	  
+	  /**
+	   * test case of Lazy Initialization singleton 
+	   */
+	  /*
+	  singletonObject1 = Singleton1.getInstance();
+	  singletonObject2 = Singleton1.getInstance();*/
+	  
+	  /**
+	   * test case of Eager Initialization singleton 
+	   */
+	  /*
+	  singletonObject1 = Singleton2.getInstance();
+	  singletonObject2 = Singleton2.getInstance();
+	  */
+	  
+	  /**
+	   * test case of Bill Pugh's singleton
+	   */
+	  singletonObject1 = Singleton3.SingletonCreator.getInstance();
+	  singletonObject2 = Singleton3.SingletonCreator.getInstance();
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
